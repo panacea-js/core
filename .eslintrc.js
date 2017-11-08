6be8ab81784faeeebd66f6487b6fde626d387016
Barry Fisher <barry.fisher@reallifedigital.com>
2017-11-08 14:49:53 +0000

    Updates eslint to understand es6 imports etc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "modules": true,
+      "experimentalObjectRestSpread": true
+    }
+  },
   "plugins": [
     "import"
   ]
