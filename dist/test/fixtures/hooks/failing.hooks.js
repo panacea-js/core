67c4b9486ada7300ad4f395d6fbcfbb24d1b1f39
Barry Fisher <barry.fisher@reallifedigital.com>
2018-10-16 02:21:56 +0100

    Remove test files from dist

--- dist/test/fixtures/hooks/failing.hooks.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.shouldOnlyExportDefault = {
-    register() {
-        // This should fail
-    }
-};
-//# sourceMappingURL=failing.hooks.js.map
