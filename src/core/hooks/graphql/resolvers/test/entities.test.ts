f6734f6c61479499bde7ea60d7fddb64dedbd08c
Barry Fisher <barry.fisher@reallifedigital.com>
2018-10-24 02:52:08 +0100

    Improve test coverage for entity resolver default value variations

--- src/core/hooks/graphql/resolvers/test/entities.test.ts
+++ src/core/hooks/graphql/resolvers/test/entities.test.ts
@@ -45,17 +45,48 @@ test.serial('Can create a Cat entity without specifying non-required fields that
 })
 
 test.serial('Can create a Cat entity without specifying non-required fields that later become required are prepopulated with default values from query', async t => {
-  t.plan(2)
+  t.plan(6)
 
   await createCat('Cat with story with missing body text', '[{ title: "Here is a title" }]')
   await createCat('Cat with story with missing title text', '[{ body: "Here is a body" }]')
 
   hooks.on('core.entityTypes.definitions', ({ definitions } : { definitions: EntityTypeDefinitions }) => {
+    // Field alterations.
     definitions.Cat.fields.stories.fields.title.required = true
     definitions.Cat.fields.stories.fields.title.default = 'Default title'
     definitions.Cat.fields.stories.fields.body.required = true
     definitions.Cat.fields.stories.fields.body.default = 'Default body'
+
+    // Additional fields.
+
+    definitions.Cat.fields.intCode = {
+      label: 'Number code with 55 default value',
+      type: 'int',
+      required: true,
+      default: 55
+    }
+
+    definitions.Cat.fields.floatCode = {
+      label: 'Float code with no specific default value',
+      type: 'float',
+      required: true
+    }
+
+    definitions.Cat.fields.longTextCode = {
+      label: 'Text code with BLAH default value',
+      type: 'text',
+      required: true,
+      default: 'BLAH'
+    }
+
+    definitions.Cat.fields.textCode = {
+      label: 'Text code with no default value',
+      type: 'string',
+      required: true
+    }
   })
+
+  hooks.invoke('core.reload', { reason: 'Flushing Cat definitions in test' })
 
   const allCatsWithDefaultValues = await allCats()
 
@@ -64,4 +95,22 @@ test.serial('Can create a Cat entity without specifying non-required fields that
 
   t.is(actualDefaultBody, 'Default body')
   t.is(actualDefaultTitle, 'Default title')
+
+  const allCatsWithNewFields = () => graphqlQuery(`{
+    cats {
+      id,
+      name,
+      intCode,
+      floatCode,
+      longTextCode,
+      textCode
+    }
+  }`)
+
+  const result = await allCatsWithNewFields()
+
+  t.is(result.data.cats[0].intCode, 55)
+  t.is(result.data.cats[0].floatCode, 0)
+  t.is(result.data.cats[0].longTextCode, 'BLAH')
+  t.is(result.data.cats[0].textCode, '')
 })
